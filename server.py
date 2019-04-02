1994cd02ad39a0aeacc52b3fa219569f77f8c114
Ben Isak <benjamin.isak@gmx.de>
2019-04-02 13:54:26 +0200

    update with colorama and pressure signaling

--- server.py
+++ server.py
@@ -1,49 +1,89 @@
-#Author: Benjamin Isak - last update 03/18/2019
+#Author: Benjamin Isak - last update 04/02/2019
 
-from opcua import Server
+from opcua import ua, Server, uamethod
 from random import randint
+from colorama import init, Fore, Style
 import datetime
 import time
+import logging
+import os
+import pyfiglet
 
 #---SERVER INIT---#
+logger = logging.getLogger("opcua.address_space")
+logger.setLevel(logging.DEBUG)
+
+#---colorama init---#
+init(autoreset=True)
+#-------------------#
+
 server = Server()
-url = "opc.tcp://localhost:4841"
+
+url = "opc.tcp://192.168.178.23:4840"
 server.set_endpoint(url)
 
 name = "OPCUA_MACHINE_SIMULATION_SERVER"
 addspace = server.register_namespace(name) #Addressraum
 
+# alle Node-Objekte kommen hier rein
 machine_node = server.get_objects_node()
 #-----------------#
 
+def getSensors():
+    print(Fore.GREEN + "\nAktuelle Sensoren: \n","-Temperatur (°C) \n", "-Druck (bar) \n")
+
 #---MASCHINEN PARAMETER / SENSOREN---#
+
+# unseren Adressraum füllen
 Params = machine_node.add_object(addspace, "Parameters")
 
 temperature = Params.add_variable(addspace, "Temperatur", 0)
-pressure = Params.add_variable(addspace, "Druck", 0)
+pressure = Params.add_variable(addspace, "Druck", 1.5)
 systemTime = Params.add_variable(addspace, "Zeitstempel", 0)
 
-temperature.set_writable()
-pressure.set_writable()
-systemTime.set_writable()
+for p in Params.get_children():
+    p.set_writable()
 
-server.start()
-print("OPC UA - Server started at {}".format(url))
+try:
+    server.start()
+    os.system("cls")
+
+    ascii_banner = pyfiglet.figlet_format("OPC UA Server")
+    print(ascii_banner)
+    print("...by Benjamin Isak ...2019\n")
+    print(Fore.LIGHTBLUE_EX + "Server started at {}".format(url))
+    time.sleep(0.5)
+
+    getSensors()
+
+    for x in range(0,8):
+        print(".")
+        time.sleep(0.2)
 
 #---SIMULATION---#
-while True:
-    Temp = randint(10, 50)
-    Press = randint(100, 999)
-    Time = datetime.datetime.now()
 
-    print("Temp °C: {} - ".format(Temp), "Druck (bar): {} - ".format(Press), Time)
+    Press = 1.50
 
-    temperature.set_value(Temp)
-    pressure.set_value(Press)
-    systemTime.set_value(Time)
+    while True:
+        Temp = randint(10, 50)
+        Press += 0.10 
+        Time = datetime.datetime.now()
 
-    time.sleep(1.5)
-    
+        print("Temp °C: {}".format(Temp))
+        if (Press >= 2.5): 
+            print(Fore.RED + "Druck (bar): {}".format(round(Press,2)))
+        else:
+             print("Druck (bar): {}".format(round(Press,2)))
+
+        print(Time, "\n")
+
+        temperature.set_value(Temp)
+        pressure.set_value(Press)
+        systemTime.set_value(Time)
+
+        time.sleep(1)
 #----------------#
 
+finally:
+    server.stop()
 
